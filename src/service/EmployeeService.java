75e22802e0f008cfb165379cb3ba6627a4af02a9
jasmine.zhu <jasmine.zhu@oocl.com>
2019-12-24 19:24:52 +0800

    feat: add method for get adult employees

--- src/service/EmployeeService.java
+++ src/service/EmployeeService.java
@@ -1,11 +1,18 @@
 package service;
 
+import dao.EmployeeDao;
 import model.Employee;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeeService {
 
   public List<Employee> getAdultEmployees() {
-    return null;
+    EmployeeDao employeeDao = new EmployeeDao();
+    List<Employee> allEmployees = employeeDao.getAllEmployees();
+    List<Employee> adultEmployees = allEmployees.stream()
+        .filter(employee -> employee.getAge() >= 18).collect(Collectors.toList());
+    return adultEmployees;
   }
 }
