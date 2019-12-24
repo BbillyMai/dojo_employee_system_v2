2d26523126038de70fc91cfca22fab8a83723ce9
jasmine.zhu <jasmine.zhu@oocl.com>
2019-12-24 19:21:16 +0800

    test: add unit test for get adult employees

---
+++ src/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.Employee;
+import java.util.List;
+
+public class EmployeeService {
+
+  public List<Employee> getAdultEmployees() {
+    return null;
+  }
+}
---
+++ test/service/EmployeeServiceTest.java
@@ -0,0 +1,23 @@
+package service;
+
+import model.Employee;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class EmployeeServiceTest {
+
+  @Test
+  public void should_return_adult_employees_when_invoke_getAdultEmployees() {
+    EmployeeService employeeService = new EmployeeService();
+    List<Employee> adultEmployees = employeeService.getAdultEmployees();
+
+    assertEquals(2, adultEmployees.size());
+    assertTrue(adultEmployees.get(0).getAge() >= 18);
+    assertTrue(adultEmployees.get(1).getAge() >= 18);
+  }
+
+}
